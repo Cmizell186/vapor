3bb0cf381194f220419c7ac208c05b2c32375ec8
cmizell186 <chrislmiz311@gmail.com>
2022-04-29 19:12:40 -0700

    maybe fixed libraries test3

--- app/seeds/libraries.py
+++ app/seeds/libraries.py
@@ -15,69 +15,69 @@ def seed_libraries():
     user1_lib2 = Library(
         user_id = 1,
         game_id = 3,
-        is_owner = True
+        is_owned = True
     )
     user1_lib3 = Library(
         user_id = 1,
         game_id = 4,
-        is_owner = True
+        is_owned = True
     )
     user1_lib4 = Library(
         user_id = 1,
         game_id = 5,
-        is_owner = True
+        is_owned = True
     )
 
     user2_cart = Library(
         user_id = 2,
         game_id = 1,
-        is_owner = False
+        is_owned = False
     )
     user2_lib = Library(
         user_id = 2,
         game_id = 2,
-        is_owner = True
+        is_owned = True
     )
     user2_lib2 = Library(
         user_id = 2,
         game_id = 3,
-        is_owner = True
+        is_owned = True
     )
     user2_lib3 = Library(
         user_id = 2,
         game_id = 4,
-        is_owner = True
+        is_owned = True
     )
     user2_lib4 = Library(
         user_id = 2,
         game_id = 5,
-        is_owner = True
+        is_owned = True
     )
 
     user3_cart = Library(
         user_id = 3,
         game_id = 1,
-        is_owner = False
+        is_owned = False
     )
     user3_cart2 = Library(
         user_id = 3,
         game_id = 2,
-        is_owner = False
+        is_owned = False
     )
     user3_lib = Library(
         user_id = 3,
         game_id = 3,
-        is_owner = True
+        is_owned = True
     )
     user3_lib2 = Library(
         user_id = 3,
         game_id = 4,
-        is_owner = True
+        is_owned = True
     )
     user3_lib3 = Library(
         user_id = 3,
         game_id = 5,
-        is_owner = True
+        is_owned = True
     )
 
     db.session.add_all([user1_cart,
