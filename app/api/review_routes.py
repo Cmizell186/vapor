369aea782c6ae109588f7e5c81c46564ec367ee9
JayDrojas <damianxrojas@gmail.com>
2022-04-30 16:59:49 -0700

    finished route and reduxer for reviewsesa

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -7,3 +7,8 @@ review_routes = Blueprint('reviews', __name__)
 def get_reviews():
   reviews_list = Review.query.all()
   return {'reviews_list': [review.to_dict() for review in reviews_list]}
+
+@review_routes.route('/<int:id>')
+def get_specific_review(id):
+  specific_review = Review.query.get(id)
+  return specific_review
