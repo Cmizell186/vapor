2566e1aaf7c4485f27cc5ed4448e2755e588f8b2
cmizell186 <chrislmiz311@gmail.com>
2022-04-29 19:04:25 -0700

    seeded data for all 3 current users

--- app/seeds/libraries.py
+++ app/seeds/libraries.py
@@ -33,3 +33,49 @@ def seed_libraries():
         game_id = 1,
         is_owner = False
     )
+    user2_lib = Library(
+        user_id = 2,
+        game_id = 2,
+        is_owner = True
+    )
+    user2_lib2 = Library(
+        user_id = 2,
+        game_id = 3,
+        is_owner = True
+    )
+    user2_lib3 = Library(
+        user_id = 2,
+        game_id = 4,
+        is_owner = True
+    )
+    user2_lib4 = Library(
+        user_id = 2,
+        game_id = 5,
+        is_owner = True
+    )
+
+    user3_cart = Library(
+        user_id = 3,
+        game_id = 1,
+        is_owner = False
+    )
+    user3_cart2 = Library(
+        user_id = 3,
+        game_id = 2,
+        is_owner = False
+    )
+    user3_lib = Library(
+        user_id = 3,
+        game_id = 3,
+        is_owner = True
+    )
+    user3_lib2 = Library(
+        user_id = 3,
+        game_id = 4,
+        is_owner = True
+    )
+    user3_lib3 = Library(
+        user_id = 3,
+        game_id = 5,
+        is_owner = True
+    )
