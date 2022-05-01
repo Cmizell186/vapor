bc5208423c40ddf0a73de05f47922457216e5819
Jared Kunhart <hakujaden@comcast.net>
2022-05-01 14:28:11 -0500

    fixed library seed, updated reviews seeds

--- app/seeds/libraries.py
+++ app/seeds/libraries.py
@@ -58,19 +58,9 @@ def seed_libraries():
         game_id = 6,
         is_owned = True
     )
-    user2_lib5 = Library(
-        user_id = 2,
-        game_id = 1,
-        is_owned = True
-    )
     user3_cart = Library(
         user_id = 3,
         game_id = 1,
-        is_owned = False
-    )
-    user3_cart2 = Library(
-        user_id = 3,
-        game_id = 2,
         is_owned = False
     )
     user3_lib = Library(
@@ -107,8 +97,8 @@ def seed_libraries():
     db.session.add_all([
     user1_cart, user1_lib, user1_lib2, user1_lib3, user1_lib4,
     user2_cart, user2_lib, user2_lib2, user2_lib3, user2_lib4,
-    user3_cart, user3_cart2, user3_lib, user3_lib2, user3_lib3,
-    user1_lib5, user2_lib5, user4_lib, user4_lib2
+    user3_cart, user3_lib, user3_lib2, user3_lib3, user1_lib5,
+    user4_lib, user4_lib2, user5_lib
     ])
 
     db.session.commit()
