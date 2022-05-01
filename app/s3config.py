5977deeb3b1d36e7e2cc7190286f00cca09732ef
cmizell186 <chrislmiz311@gmail.com>
2022-05-01 10:47:05 -0700

    finished setting up s3config.py

--- app/s3config.py
+++ app/s3config.py
@@ -3,6 +3,12 @@ import botocore
 import os
 import uuid
 
+
+BUCKET_NAME = os.environ.get("S3_BUCKET")
+S3_LOCATION = f"http://vaporgames.s3.amazonaws.com/"
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+
 s3 = boto3.client(
    "s3",
    aws_access_key_id=os.environ.get("S3_KEY"),
@@ -10,11 +16,8 @@ s3 = boto3.client(
 )
 
 
-
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-
 def allowed_file(filename):
-    return '.' in filename and \
+    return "." in filename and \
         filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def get_unique_filename(filename):
@@ -24,3 +27,20 @@ def get_unique_filename(filename):
 
 
 
+
+def upload_file_to_s3(file, acl="public-read"):
+    try:
+        s3.upload_fileobj(
+            file,
+            BUCKET_NAME,
+            file.filename,
+            ExtraArgs={
+                "ACL": acl,
+                "ContentType": file.content_type
+            }
+        )
+    except Exception as e:
+        # in case the our s3 upload fails
+        return {"errors": str(e)}
+
+    return {"url": f"{S3_LOCATION}{file.filename}"}
