cda1bf2fe0abfd715c8afa9827e8c383c1bcb039
JayDrojas <damianxrojas@gmail.com>
2022-05-01 21:09:08 -0700

    added css to GameDetail component

--- react-app/src/components/Games/GameDetail.js
+++ react-app/src/components/Games/GameDetail.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { useParams } from "react-router-dom";
 import React, { useEffect, useState } from 'react';
 import ReviewGame from "../Reviews/ReviewsForm";
+import './index.css'
 
 const GameDetails = () => {
   const { gameId } = useParams();
@@ -22,31 +23,42 @@ const GameDetails = () => {
 
   return (
     <>
-      <div>
-        <h2>{game?.title}</h2>
-        <p>{game?.description}</p>
-        <p>{game?.price}</p>
-        <p>{new Date(game?.release_date).toLocaleDateString('en-US', DATE_OPTIONS)}</p>
-        <p>{game?.is_mature}</p>
-        <p>{game?.video}</p>
-        <p>{game?.img}</p>
-        <img src={game?.img} alt=""/>
-        <p>{game?.developer}</p>
-      </div>
-      <div className='create-reviews-container'>
-        <ReviewGame gameId={gameId}/>
-      </div>
-      <div className='reviews-container'>
-      { filteredReviews?.map(review =>
-        <div key={review.id}>
-          <h2>
-            {review.content}
-          </h2>
+      <div id="page-content-container">
+        <div id="title-container">
+          <h2>{game?.title}</h2>
         </div>
-      )}
+        <div id="image-details-container">
+          <div id="react-media-subcontainer">
+            <img id="selected-image" src={'https://community.clover.com/themes/base/admin/img/default-coverImage.png'} alt="" />
+          </div>
+          <div id="details-subcontainer">
+            <div>
+              <img id="main-game-image" src='https://community.clover.com/themes/base/admin/img/default-coverImage.png' alt="" />
+            </div>
+            <p>{game?.description}</p>
+            <p>{game?.price}</p>
+            <p>{new Date(game?.release_date).toLocaleDateString('en-US', DATE_OPTIONS)}</p>
+            <p>{game?.is_mature}</p>
+            <p>{game?.video}</p>
+            <p>{game?.img}</p>
+            <p>{game?.developer}</p>
+          </div>
+        </div>
+        <div className='create-reviews-container'>
+          <ReviewGame gameId={gameId} />
+        </div>
+        <div className='reviews-container'>
+          {filteredReviews?.map(review =>
+            <div key={review.id}>
+              <h2>
+                {review.content}
+              </h2>
+            </div>
+          )}
+        </div>
       </div>
     </>
   )
-  }
+}
 
 export default GameDetails;
