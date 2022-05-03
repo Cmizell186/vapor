28686e2f34a301f8d9d604bf2b1f8146128d5e31
Jared Kunhart <hakujaden@comcast.net>
2022-05-02 20:19:43 -0500

    navbar done

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import LogoutButton from './auth/LogoutButton';
+import ProfileMenu from '../components/Profile/ProfileMenu'
+import '../../src/components/LandingPage/index.css'
 
 const NavBar = ({user}) => {
+  console.log(user)
   return (
     <div className='splashWrapper'>
       <div id="nav">
         <div id="navarea">
         <div id="div_logo"><img src="/images/vapor_logo.png" id="logo"></img></div>
         <div id="navbar">
-                <div className="nav_nav" id="nav_store">STORE</div>
-                <div className="nav_nav" id="nav_library">LIBRARY</div>
-                <div className="nav_nav" id="nav_profile">PROFILE</div>
-                <div className="nav_nav" id="nav_about">ABOUT</div>
+                <div className="nav_nav" id="nav_store"><a className='nav_links' href='/games'>STORE</a></div>
+                <div className="nav_nav" id="nav_library"><a className='nav_links' href='/games'>LIBRARY</a></div>
+                <div className="nav_nav" id="nav_profile"><a className='nav_links' href={`/users/${user.id}`}>PROFILE</a></div>
+                <div className="nav_nav" id="nav_about"><a className='nav_links' href='/games'>ABOUT</a></div>
           </div>
-          {user ? <div id="loginnav"> {user.username} <LogoutButton /> </div> :
+          {user ? <div id="loginnav"><ProfileMenu user={user} /><div id='profile_image'><img src={user.profile_picture} id="profile_pic" alt=""></img></div></div> :
           <div id="loginnav">Login | <a href="/demo" id="demo_login">Demo</a></div>}
         </div>
       </div>
