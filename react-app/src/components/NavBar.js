7ec3491b758f007720fd2b59fe2330cfdca5c7a9
Jared Kunhart <hakujaden@comcast.net>
2022-05-02 20:27:42 -0500

    fix for logged out

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -12,11 +12,13 @@ const NavBar = ({user}) => {
         <div id="navbar">
                 <div className="nav_nav" id="nav_store"><a className='nav_links' href='/games'>STORE</a></div>
                 <div className="nav_nav" id="nav_library"><a className='nav_links' href='/games'>LIBRARY</a></div>
-                <div className="nav_nav" id="nav_profile"><a className='nav_links' href={`/users/${user.id}`}>PROFILE</a></div>
+                {user ? <div className="nav_nav" id="nav_profile"><a className='nav_links' href={`/users/${user.id}`}>PROFILE</a></div> :
+                <></>}
+
                 <div className="nav_nav" id="nav_about"><a className='nav_links' href='/games'>ABOUT</a></div>
           </div>
           {user ? <div id="loginnav"><ProfileMenu user={user} /><div id='profile_image'><img src={user.profile_picture} id="profile_pic" alt=""></img></div></div> :
-          <div id="loginnav">Login | <a href="/demo" id="demo_login">Demo</a></div>}
+          <div id="loginnav">login |  <a href="/demo" id="demo_login">demo</a></div>}
         </div>
       </div>
     </div>
