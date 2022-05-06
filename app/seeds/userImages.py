3183c58f7d93627a248b1377334bf87472bd03dd
cmizell186 <chrislmiz311@gmail.com>
2022-05-06 10:20:47 -0700

    finished seeding user images

---
+++ app/seeds/userImages.py
@@ -0,0 +1,37 @@
+from app.models import db
+from app.models.userImage import UserImage
+
+def seed_userImages():
+
+    demo = UserImage(
+        id=1,
+        image='https://vaporgames.s3.us-west-1.amazonaws.com/default_user.jpg',
+        user_id=1
+    )
+    damian_user = UserImage(
+        id=2,
+        image='https://vaporgames.s3.us-west-1.amazonaws.com/damianpfp.jpg',
+        user_id=2
+    )
+    jason_user = UserImage(
+        id=3,
+        image="https://vaporgames.s3.us-west-1.amazonaws.com/jasonpfp.jpg",
+        user_id=3
+    )
+    chris_user = UserImage(
+        id=4,
+        image="https://vaporgames.s3.us-west-1.amazonaws.com/THELASTTIMEISEENPHOTO.jpg",
+        user_id=4
+    )
+    jared_user = UserImage(
+        id=5,
+        image="https://vaporgames.s3.us-west-1.amazonaws.com/jaredprofilepic.png",
+        user_id=5
+    )
+
+    db.session.add_all([demo, damian_user, jason_user, chris_user, jared_user])
+    db.session.commit()
+
+def undo_userImages():
+    db.session.execute('TRUNCATE userImages RESTART IDENTITY CASCADE;')
+    db.session.commit()
