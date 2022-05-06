cf83c8c1da9c6770c98f8b7c14f8fb62f6934f20
Jared Kunhart <hakujaden@comcast.net>
2022-05-06 02:43:33 -0500

    library started

--- react-app/src/components/Library/index.js
+++ react-app/src/components/Library/index.js
@@ -13,14 +13,27 @@ const Library = () => {
 
   const DATE_OPTIONS = { year: 'numeric', month: 'short', day: 'numeric' };
 // {new Date(game?.release_date).toLocaleDateString('en-US', DATE_OPTIONS)}
+// 6th picture? should be 600x900
   return (
+    <div id="library_container">
+    <div id="library_content">
     <div id="library">
-{games?.map(game =>
+    {games?.map(game =>
     <div className="games" key={game?.id}>
+    <div id="game_tile">
+    <a href={`/games/${game.id}`}>
     <div class="game"><img src={game?.images[0]?.image} alt="" /></div>
-    <div class="game"><img src="https://steamcdn-a.akamaihd.net/steam/apps/39140/library_600x900.jpg" alt="" /></div>
+    <div id="library_game_title_div">
+        <div id="library_game_title">
+        {game?.title}
+        </div>
+    </div>
+    </a>
+    </div>
     </div>
 )}
+</div>
+</div>
 </div>
   )
 }
