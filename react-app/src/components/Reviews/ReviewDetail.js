dc943062e87d202280e3d40353a6103139f66b78
Jared Kunhart <hakujaden@comcast.net>
2022-05-06 17:14:59 -0500

    breath of the wild

--- react-app/src/components/Reviews/ReviewDetail.js
+++ react-app/src/components/Reviews/ReviewDetail.js
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from "react";
 import { get_one_review } from "../../store/reviews";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams, useHistory } from "react-router-dom";
-// import ReviewEditModal from "./ReviewsEditModal";
-import EditReview from "./ReviewsEditForm";
 import { delete_review } from "../../store/reviews";
 import { Modal } from "../../context/Modal";
+import { update_review } from "../../store/reviews";
+import './editstyles.css'
 
 import "./index.css";
 
@@ -18,10 +18,41 @@ const ReviewDetails = ({ loaded }) => {
   const sessionUser = useSelector((state) => state.session.user);
   const [showModal, setShowModal] = useState(false);
   const [formDiv, setFormDiv] = useState(false);
+  const [is_recommended, setIs_Recommended] = useState(
+    review?.is_recommended
+  );
+  const [content, setContent] = useState(review?.content);
+  const [errors, setErrors] = useState([]);
+  const [hasSubmitted, setHasSubmitted] = useState(false);
+  const [style_rec_yes, setStyle_Rec_Yes] = useState("recommend_yes");
+  const [style_rec_no, setStyle_Rec_No] = useState("recommend_no")
+
+  let ourErrors = [];
+  if (!content) ourErrors.push("Please provide what you liked or disliked about this game and whether you recommend it to others.")
+  if (is_recommended === null) ourErrors.push("Please provide either recommended or not recommended.")
+  // if (ourErrors.length) {
+  //   return setErrors(ourErrors)
+  // }
 
   useEffect(() => {
     dispatch(get_one_review(reviewId)); //warning here- useEffect has a missing dependency: 'reviewId'. Either include it or remove the dependency array
   }, [dispatch]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setHasSubmitted(true);
+    const updatedReview = {
+      id: review.id,
+      is_recommended,
+      content,
+      game_id: gameId,
+      userId: sessionUser.id,
+    };
+    await dispatch(update_review(updatedReview));
+    setIs_Recommended(false);
+    setContent("");
+    setHasSubmitted(false);
+  };
 
   const recommendation = () => {
     let recommend = review?.is_recommended;
@@ -34,20 +65,106 @@ const ReviewDetails = ({ loaded }) => {
     history.push(`/games/${review.game_id}`);
   };
 
-  const showForm = async () => {
-    let show = document.getElementById("left-offset-review-content-subbody");
-    if (show.style.display === "none") {
-      show.style.display = "block";
-      setFormDiv(true);
-    } else {
-      show.style.display = "none";
-      setFormDiv(false);
-      return (
-        <>
-          <EditReview  review={review} gameId={gameId} />
-        </>
-      )
-    }
+  const ShowReview = () => {
+    return (
+      <>
+      <div className="review-rating-subbody">
+        No one has rated this review as helpful yet
+      </div>
+      <div className="recommendation-subbody">
+        <div className="recommendation-summary">
+          <img
+            src={
+              review?.is_recommended
+                ? "https://store.cloudflare.steamstatic.com/public/shared/images/userreviews/icon_thumbsUp.png"
+                : "https://community.cloudflare.steamstatic.com/public/shared/images/userreviews/icon_thumbsDown.png?v=1"
+            }
+            width="44"
+            height="44"
+            alt=""
+          />
+        </div>
+        <div className="review-rating">{recommendation()}</div>
+      </div>
+      <div className="recommendation-created-at"></div>
+      <div className="review-detail-content">
+        <p>{review?.content}</p>
+      </div>
+      <div className="review_comments_header">
+        Comments are disabled for this review.
+      </div>
+      </>
+    )
+  }
+
+  const EditReview = ({review, gameId}) => {
+    return (
+      <>
+      <div className="review-form-container">
+        <form onSubmit={handleSubmit} className="review-content-container">
+          <div className="content-div">
+            <label htmlFor='content'>
+            </label>
+            <textarea
+              className='create_textbox'
+              type='text'
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
+          <div className="review_controls_body">
+          <div className="review_controls_container">
+            <span>visibility</span>
+          </div>
+          <div className="is_recommended-div">Do you recommend this game?</div>
+          <div className='vote_up_down_container'>
+            <button className={style_rec_yes}
+              type='button'
+              value={is_recommended}
+              onClick={(e) => {
+                setIs_Recommended(true)
+                setStyle_Rec_Yes("recommend_clicked")
+                setStyle_Rec_No("recommend_no")
+              }}
+            >
+              <span>
+              <i className="fa-solid fa-thumbs-up"></i>
+                Yes
+              </span>
+            </button>
+            <button className={style_rec_no}
+              type='button'
+              value={is_recommended}
+              onClick={(e) => {
+                setIs_Recommended(false)
+                setStyle_Rec_No("recommend_clicked")
+                setStyle_Rec_Yes("recommend_yes")
+              }}
+            >
+              <span>
+              <i className="fa-solid fa-thumbs-down"></i>
+                No
+              </span>
+            </button>
+          <button className="button btn-submit-review" type="submit">
+            <span>
+            Update Review
+            </span>
+          </button>
+          </div>
+          </div>
+          </div>
+        </form>
+      </div>
+    </>
+    )
+  }
+
+  const ShowForm = () => {
+    let review = document.getElementById("left-offset-review-content-text");
+    let editreview = document.getElementById("left-offset-review-content-edit");
+    review.style.display = "none";
+    editreview.style.display = "block"
+    // setFormDiv(false);
   };
 
   return (
@@ -92,34 +209,8 @@ const ReviewDetails = ({ loaded }) => {
         </div>
         <div className="main-content-container">
           <div className="review-detail-container">
-            <div id="left-offset-review-content-subbody">
-              <div className="review-rating-subbody">
-                No one has rated this review as helpful yet
-              </div>
-              <div className="recommendation-subbody">
-                <div className="recommendation-summary">
-                  <img
-                    src={
-                      review?.is_recommended
-                        ? "https://store.cloudflare.steamstatic.com/public/shared/images/userreviews/icon_thumbsUp.png"
-                        : "https://community.cloudflare.steamstatic.com/public/shared/images/userreviews/icon_thumbsDown.png?v=1"
-                    }
-                    width="44"
-                    height="44"
-                    alt=""
-                  />
-                </div>
-                <div className="review-rating">{recommendation()}</div>
-              </div>
-              <div className="recommendation-created-at"></div>
-              <div className="review-detail-content">
-                <p>{review?.content}</p>
-              </div>
-              <div className="review_comments_header">
-                Comments are disabled for this review.
-              </div>
-            </div>
-            {/* {!formDiv && <EditReview review={review} gameId={gameId} />} */}
+          <div id="left-offset-review-content-text" style={{display: "block"}}><ShowReview /></div>
+          <div id="left-offset-review-content-edit" style={{display: "none"}}><EditReview  review={review} gameId={gameId} /></div>
             <div className="right-offset-detail-subbody">
               <div className="game-review-links">
                 <div className="game_logo_img">
@@ -154,7 +245,7 @@ const ReviewDetails = ({ loaded }) => {
                       src="https://community.akamai.steamstatic.com/public/images/sharedfiles/icons/icon_edit.png"
                       alt=""
                     />
-                    <a href="#" onClick={() => showForm()}>
+                    <a href="#" onClick={() => ShowForm()}>
                       Update Review
                     </a>
                   </div>
