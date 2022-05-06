a9110ac58e9e2292377c1ea5f3de11842dcb47f2
cmizell186 <chrislmiz311@gmail.com>
2022-05-06 16:53:49 -0700

    finished 404 page

--- react-app/src/components/ErrorPage/ErrorPage.js
+++ react-app/src/components/ErrorPage/ErrorPage.js
@@ -1,11 +1,18 @@
 import React from "react";
-
+import "./ErrorPage.css"
 const ErrorPage = () =>{
     return (
-        <>
-            <h2>404 Page not found!</h2>
-            <a href="/">Return back home!</a>
-        </>
+        <div className="error-form-container">
+            <div className="error-div-h2">
+                <h2 className="error-h2">404</h2>
+            </div>
+            <div id="error-div-a">
+                <a href="/" className="error-link">
+                Return back home!
+                </a>
+            </div>
+            <img src="https://vaporgames.s3.us-west-1.amazonaws.com/404errorpage.png" alt="oopsie" />
+        </div>
     )
 }
 
