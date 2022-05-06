ef6df48819a00707d77589964c751f456144f42d
cmizell186 <chrislmiz311@gmail.com>
2022-05-06 15:32:37 -0700

    created a little error page

---
+++ react-app/src/components/ErrorPage/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ErrorPage = () =>{
+    return (
+        <>
+            <h2>404 Page not found!</h2>
+            <a href="/">Return back home!</a>
+        </>
+    )
+}
+
+export default ErrorPage
