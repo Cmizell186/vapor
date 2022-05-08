98a705366e81fd92096b8fa30dfa463e064cfc8c
JayDrojas <damianxrojas@gmail.com>
2022-05-07 18:22:21 -0700

    currently working except for button starting at the correct spot

--- react-app/src/components/Reviews/ReviewDetail.js
+++ react-app/src/components/Reviews/ReviewDetail.js
@@ -15,26 +15,15 @@ const ReviewDetails = ({ loaded }) => {
   const { gameId } = useParams();
   const { reviewId } = useParams();
   const review = useSelector((state) => state.reviews[reviewId]);
-  console.log(review, "88888888888888888888888")
   const sessionUser = useSelector((state) => state.session.user);
   const [showModal, setShowModal] = useState(false);
+  const [errorModal, setErrorModal] = useState(false);
   const [formDiv, setFormDiv] = useState(false);
-  console.log(formDiv, "formDiv1")
-  const [is_recommended, setIs_Recommended] = useState(
-    review?.is_recommended
-  );
+  const [is_recommended, setIs_Recommended] = useState(review?.is_recommended);
   const [content, setContent] = useState(review?.content);
   const [errors, setErrors] = useState([]);
   const [hasSubmitted, setHasSubmitted] = useState(false);
-  const [style_rec_yes, setStyle_Rec_Yes] = useState("recommend_yes");
-  const [style_rec_no, setStyle_Rec_No] = useState("recommend_no")
 
-  let ourErrors = [];
-  if (!content) ourErrors.push("Please provide what you liked or disliked about this game and whether you recommend it to others.")
-  if (is_recommended === null) ourErrors.push("Please provide either recommended or not recommended.")
-  // if (ourErrors.length) {
-  //   return setErrors(ourErrors)
-  // }
 
   useEffect(() => {
     dispatch(get_one_review(reviewId)); //warning here- useEffect has a missing dependency: 'reviewId'. Either include it or remove the dependency array
@@ -42,8 +31,18 @@ const ReviewDetails = ({ loaded }) => {
   }, [dispatch]);
 
   const handleSubmit = async (e) => {
+    console.log("inside of handle Submit")
     e.preventDefault();
     setHasSubmitted(true);
+
+    let ourErrors = [];
+    if (!content) ourErrors.push("Please provide what you liked or disliked about this game and whether you recommend it to others.")
+    if (is_recommended === null) ourErrors.push("Please provide either recommended or not recommended.")
+    if (ourErrors.length) {
+      setErrorModal(true)
+      return setErrors(ourErrors)
+    }
+
     const updatedReview = {
       id: review.id,
       is_recommended,
@@ -52,14 +51,9 @@ const ReviewDetails = ({ loaded }) => {
       userId: sessionUser.id,
     };
     await dispatch(update_review(updatedReview));
-    setIs_Recommended(false);
-    setContent("");
-    setHasSubmitted(false);
-    ShowForm()
-    setFormDiv(false)
+
     history.push(`/games/${review.game_id}`)
   };
-  console.log(formDiv, "formDiv2")
 
   const recommendation = () => {
     let recommend = review?.is_recommended;
@@ -72,123 +66,137 @@ const ReviewDetails = ({ loaded }) => {
     history.push(`/games/${review.game_id}`);
   };
 
-    // Needs the if else again to close form on update
-    const ShowForm = () => {
-      let review = document.getElementById("left-offset-review-content-text");
-      let editreview = document.getElementById("left-offset-review-content-edit");
-      if (formDiv)  {
-        setFormDiv(false)
-        review.style.display = "block"
-        return editreview.style.display = "none"
+  // Needs the if else again to close form on update
+  const ShowForm = () => {
+    let review = document.getElementById("left-offset-review-content-text");
+    let editreview = document.getElementById("left-offset-review-content-edit");
+    if (formDiv) {
+      setFormDiv(false)
+      review.style.display = "block"
+      return editreview.style.display = "none"
 
-      } if (!formDiv)
+    } if (!formDiv)
       review.style.display = "none"
-      editreview.style.display = "block"
-      console.log("heeeeerrrrre in !formDiv")
-      return setFormDiv(true)
-    };
+    editreview.style.display = "block"
+    return setFormDiv(true)
+  };
 
   const ShowReview = () => {
     return (
       <>
-      <div className="review-rating-subbody">
-        No one has rated this review as helpful yet
-      </div>
-      <div className="recommendation-subbody">
-        <div className="recommendation-summary">
-          <img
-            src={
-              review?.is_recommended
-                ? "https://store.cloudflare.steamstatic.com/public/shared/images/userreviews/icon_thumbsUp.png"
-                : "https://community.cloudflare.steamstatic.com/public/shared/images/userreviews/icon_thumbsDown.png?v=1"
-            }
-            width="44"
-            height="44"
-            alt=""
-          />
+        {hasSubmitted && errorModal && (
+          <Modal onClose={() => setErrorModal(false)}>
+            <div className="modal-content-text">
+              <h2>UPDATE YOUR REVIEW FOR</h2>
+              <div className="error-modal">
+                {errors.map((error, index) => (
+                  <div key={index}>{error}</div>
+                ))}
+              </div>
+              <div className="modal-content-bttn-ok">
+                <span onClick={e => setErrorModal(false)}>OK</span>
+              </div>
+            </div>
+
+          </Modal>
+        )}
+        <div className="review-rating-subbody">
+          No one has rated this review as helpful yet
         </div>
-        <div className="review-rating">{recommendation()}</div>
-      </div>
-      <div className="recommendation-created-at"></div>
-      <div className="review-detail-content">
-        <p>{review?.content}</p>
-      </div>
-      <div className="review_comments_header">
-        Comments are disabled for this review.
-      </div>
+        <div className="recommendation-subbody">
+          <div className="recommendation-summary">
+            <img
+              src={
+                review?.is_recommended
+                  ? "https://store.cloudflare.steamstatic.com/public/shared/images/userreviews/icon_thumbsUp.png"
+                  : "https://community.cloudflare.steamstatic.com/public/shared/images/userreviews/icon_thumbsDown.png?v=1"
+              }
+              width="44"
+              height="44"
+              alt=""
+            />
+          </div>
+          <div className="review-rating">{recommendation()}</div>
+        </div>
+        <div className="recommendation-created-at"></div>
+        <div className="review-detail-content">
+          <p>{review?.content}</p>
+        </div>
+        <div className="review_comments_header">
+          Comments are disabled for this review.
+        </div>
       </>
     )
   }
-  console.log(formDiv, "formDiv3")
   const handleChange = (e) => {
-   return setContent(e.target.value)
+    return setContent(e.target.value)
   }
 
-  const EditReview = ({review, gameId}) => {
+  const EditReview = ({ review, gameId }) => {
     return (
       <>
-      <div className="review-edit-container">
-        <form onSubmit={handleSubmit} className="review-content-container">
-        <div className="recommend_control_box">
-          <div className="is_recommended-div">Do you recommend this game?</div>
-          <div className='edit_vote_up_down_container'>
-            <button className={style_rec_yes}
-              type='button'
-              defaultValue={content}
-              onClick={(e) => {
-                setIs_Recommended(true)
-                setStyle_Rec_Yes("recommend_clicked")
-                setStyle_Rec_No("recommend_no")
-              }}
-            >
-              <span>
-              <i className="fa-solid fa-thumbs-up"></i>
-                Yes
-              </span>
-            </button>
-            <button className={style_rec_no}
-              type='button'
-              defaultValue={review?.is_recommended}
-              onClick={(e) => {
-                setIs_Recommended(false)
-                setStyle_Rec_No("recommend_clicked")
-                setStyle_Rec_Yes("recommend_yes")
-              }}
-            >
-              <span>
-              <i className="fa-solid fa-thumbs-down"></i>
-                No
-              </span>
-            </button>
-            </div>
-          <div className="content-div">
-            <label htmlFor='content'>
-            </label>
-            <textarea
-              key={review?.id}
-              className='create_edit_textbox'
-              type='text'
-              defaultValue={content}
-              // value={review?.content}
-              placeholder={review?.content}
-              onBlur={handleChange}
-            />
-          <div className="review_controls_body">
-          <div className="review_controls_container">
-            <span>visibility</span>
-          </div>
+        <div className="review-edit-container">
+          <form onSubmit={handleSubmit} className="review-content-container">
+            <div className="recommend_control_box">
+              <div className="is_recommended-div">Do you recommend this game?</div>
+              <div className='edit_vote_up_down_container'>
+                <button className={is_recommended ? 'recommend_clicked' : 'recommend_yes'}
+                  type='button'
+                  // defaultValue={content}
+                  onClick={(e) => {
+                    setIs_Recommended(true)
+                    // setStyle_Rec_Yes("recommend_clicked")
+                    // setStyle_Rec_No("recommend_no")
+                  }}
+                >
+                  <span>
+                    <i className="fa-solid fa-thumbs-up"></i>
+                    Yes
+                  </span>
+                </button>
+                <button className={!is_recommended ? 'recommend_clicked' : 'recommend_no'}
+                  type='button'
+                  // defaultValue={review?.is_recommended}
+                  onClick={(e) => {
+                    setIs_Recommended(false)
+                    // setStyle_Rec_No("recommend_clicked")
+                    // setStyle_Rec_Yes("recommend_yes")
+                  }}
+                >
+                  <span>
+                    <i className="fa-solid fa-thumbs-down"></i>
+                    No
+                  </span>
+                </button>
+              </div>
+              <div className="content-div">
+                <label htmlFor='content'>
+                </label>
+                <textarea
+                  key={review?.id}
+                  className='create_edit_textbox'
+                  type='text'
+                  defaultValue={content}
+                  // value={review?.content}
+                  placeholder={review?.content}
+                  onBlur={handleChange}
+                />
+                <div className="review_controls_body">
+                  <div className="review_controls_container">
+                    <span>visibility</span>
+                  </div>
 
-          <button className="button btn-submit-review" type="submit">
-            <span>
-            Update Review
-            </span>
-          </button>
-          </div>
-          </div>
-          </div>
-        </form>
-      </div>
-    </>
+                  <button className="button btn-submit-review" type="submit">
+                    <span>
+                      Update Review
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </>
     )
   }
   console.log(formDiv, "formDiv after editReview")
@@ -235,8 +243,8 @@ const ReviewDetails = ({ loaded }) => {
         </div>
         <div className="main-content-container">
           <div className="review-detail-container">
-          <div id="left-offset-review-content-text" style={{display: "block"}}><ShowReview /></div>
-          <div id="left-offset-review-content-edit" style={{display: "none"}}><EditReview  review={review} gameId={gameId} /></div>
+            <div id="left-offset-review-content-text" style={{ display: "block" }}><ShowReview /></div>
+            <div id="left-offset-review-content-edit" style={{ display: "none" }}><EditReview review={review} gameId={gameId} /></div>
             <div className="right-offset-detail-subbody">
               <div className="game-review-links">
                 <div className="game_logo_img">
@@ -292,21 +300,21 @@ const ReviewDetails = ({ loaded }) => {
                   {showModal && (
                     <Modal onClose={() => setShowModal(false)}>
                       <div className="delete-review-form">
-                      <h2>DELETE REVIEW?</h2>
-                      <p>
-                        Are you sure you want to delete this review? This cannot
-                        be undone
-                      </p>
-                      <div className="modal-content-bttns">
-                        <span onClick={() => handleDelete(review.id)}>
-                          {" "}
-                          Ok{" "}
-                        </span>
-                        <span onClick={() => setShowModal(false)}>
-                          {" "}
-                          Cancel{" "}
-                        </span>
-                      </div>
+                        <h2>DELETE REVIEW?</h2>
+                        <p>
+                          Are you sure you want to delete this review? This cannot
+                          be undone
+                        </p>
+                        <div className="modal-content-bttns">
+                          <span onClick={() => handleDelete(review.id)}>
+                            {" "}
+                            Ok{" "}
+                          </span>
+                          <span onClick={() => setShowModal(false)}>
+                            {" "}
+                            Cancel{" "}
+                          </span>
+                        </div>
                       </div>
                     </Modal>
                   )}
